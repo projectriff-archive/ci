16f297c494319aaec59454d930f243c9c8f6f22d
Felix Reyn <freyn@pivotal.io>
2017-10-23 15:32:28 -0400

    Dump heap on OOME
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- tasks/scripts/build_sk8s.sh
+++ tasks/scripts/build_sk8s.sh
@@ -13,7 +13,7 @@ cp -r $build_root/git-kubernetes-model $model_dir/kubernetes-model
 cd $model_dir/kubernetes-model
 
 #export MAVEN_OPTS="-Xms512m -Xmx1024m -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=1024M -XX:MaxPermSize=128m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC"
-export MAVEN_OPTS="-XX:-UseGCOverheadLimit"
+export MAVEN_OPTS="-XX:-UseGCOverheadLimit -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/root"
 
 make
 cd $build_root/git-sk8s
