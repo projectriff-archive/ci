753ee7f6dbd4bf6f06d4a053450c5aafef378f2f
Felix Reyn <freyn@pivotal.io>
2017-10-23 15:15:18 -0400

    Bump concourse worker size; more jvmargs for sk8s build
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- tasks/scripts/build_sk8s.sh
+++ tasks/scripts/build_sk8s.sh
@@ -12,7 +12,7 @@ mkdir -p $model_dir
 cp -r $build_root/git-kubernetes-model $model_dir/kubernetes-model
 cd $model_dir/kubernetes-model
 
-export MAVEN_OPTS="-Xms512m -Xmx1024m -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=1024M -XX:+CMSClassUnloadingEnabled"
+export MAVEN_OPTS="-Xms512m -Xmx1024m -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=1024M -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC"
 
 make
 cd $build_root/git-sk8s
