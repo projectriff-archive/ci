75b021ea94e0660e33ee5f21a869d8ed49640aff
Felix Reyn <freyn@pivotal.io>
2017-10-23 14:38:01 -0400

    More jvm options for maven GC issues
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- tasks/scripts/build_sk8s.sh
+++ tasks/scripts/build_sk8s.sh
@@ -11,10 +11,11 @@ model_dir="$(go env GOPATH)/src/github.com/fabric8io"
 mkdir -p $model_dir
 cp -r $build_root/git-kubernetes-model $model_dir/kubernetes-model
 cd $model_dir/kubernetes-model
-make
-cd $build_root/git-sk8s
 
 export MAVEN_OPTS="-Xms256m -Xmx256m -XX:MetaspaceSize=256M -XX:MaxMetaspaceSize=256M -XX:+CMSClassUnloadingEnabled"
+
+make
+cd $build_root/git-sk8s
 
 ./mvnw clean package
 ./dockerize
