6424872be95ef0b799578e2aff2a328e5a1be7dc
Jamie Klassen <cklassen@pivotal.io>
2017-10-25 14:44:58 -0400

    Deploy sk8s via helm
    - Clean up any existing helm deployments and corresponding namespaces
    - Init a helm tiller in a dedicated namespace
    - Package sk8s chart
    - Deploy sk8s chart
    
    [https://github.com/pivotal-cf/pfs-ci/issues/10]
    
    Signed-off-by: Felix Reyn <freyn@pivotal.io>

--- tasks/scripts/build_sk8s.sh
+++ tasks/scripts/build_sk8s.sh
@@ -12,28 +12,66 @@ cd $build_root/git-sk8s
 
 start_docker
 
+
+# --- build --- #
+
+docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+
 ./mvnw clean package
 ./dockerize
+./mvnw dockerfile:push -pl \
+topic-controller,\
+topic-gateway,\
+event-dispatcher,\
+function-sidecar,\
+zipkin-server,\
+function-invokers/java-function-invoker,\
+function-invokers/node-function-invoker,\
+function-invokers/python2-function-invoker,\
+function-invokers/shell-function-invoker
+
+pushd charts
+  helm package sk8s
+  # TODO: push chart to helm repo
+popd
+
+
+# --- deploy --- #
 
 mkdir ~/.kube
 echo "$KUBECONFIG_STRING" > ~/.kube/config
 
-set +e
-existing_ns=$(kubectl get ns -o json | jq -r  .items[].metadata.name | grep "$K8S_NS_PREFIX")
-
-if [ ! -z "$existing_ns" ]; then
-  kubectl delete ns "$existing_ns" --cascade=true
-  sleep 30
+existing_tiller_ns_name=$(kubectl get ns | grep "$K8S_NS_PREFIX-tiller")
+if [ ! -z "$existing_tiller_ns_name" ]; then
+  helm ls  --tiller-namespace="$existing_tiller_ns_name" | grep -v NAME | awk '{print $1}' | xargs -I{} helm  --tiller-namespace="$existing_tiller_ns_name" delete {} --purge
 fi
-set -e
+
+kubectl get ns -o json | jq -r  .items[].metadata.name | grep "$K8S_NS_PREFIX" | xargs -I{} kubectl delete ns {} --cascade=true
+sleep 30
 
 ns_suffix=$(date "+%s")
-ns_name="$K8S_NS_PREFIX"-"$ns_suffix"
+tiller_ns_name="$K8S_NS_PREFIX"-tiller-"$ns_suffix"
+sk8s_ns_name="$K8S_NS_PREFIX"-sk8s-"$ns_suffix"
+helm_release_name="sk8s-$ns_suffix"
 
-kubectl create ns "$ns_name"
-kubectl apply -f config/types -n "$ns_name"
-kubectl apply -f config/kafka -n "$ns_name"
-kubectl apply -f config -n "$ns_name"
+kubectl create ns "$tiller_ns_name"
+kubectl create ns "$sk8s_ns_name"
+
+helm init --tiller-namespace="$tiller_ns_name"
+
+# clear out existing CRDs; safe to do in non-prod
+kubectl get customresourcedefinitions --all-namespaces -o json |
+  jq -r  .items[].metadata.name |
+  xargs -I{} kubectl delete customresourcedefinition {}
+
+pushd charts
+    chart_file=$(basename sk8s*tgz)
+    helm install "$chart_file" \
+      --tiller-namespace="$tiller_ns_name" \
+      --namespace="$sk8s_ns_name" \
+      --name="$helm_release_name" \
+      --set create.faas=true,create.crd=true
+popd
 
 pushd $build_root/git-sk8s/function-invokers/java-function-invoker/target/
     invoker_jar_file=$(readlink -f $(ls *.jar | grep -v docker-info))
