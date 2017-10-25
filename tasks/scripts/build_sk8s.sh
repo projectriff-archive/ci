8db306f724de784b5a9fac1e25cda123cafc720d
Jamie Klassen <cklassen@pivotal.io>
2017-10-25 16:49:42 -0400

    Typos and corrections
    Signed-off-by: Felix Reyn <freyn@pivotal.io>

--- tasks/scripts/build_sk8s.sh
+++ tasks/scripts/build_sk8s.sh
@@ -10,5 +10,5 @@ cp -r "$build_root/m2-repo/repository" ~/.m2/
 
 pushd $build_root/git-sk8s
   ./mvnw clean package -Ddocker.skip=true
-  cp -r $build_root/git-sk8s/* "$build_root/$sk8s-docker-contexts/"
+  cp -r $build_root/git-sk8s/* "$build_root/sk8s-docker-contexts/"
 popd
