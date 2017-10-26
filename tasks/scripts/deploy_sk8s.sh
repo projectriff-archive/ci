781699f3ca1091847d3754d289e85cfb56862806
Jamie Klassen <cklassen@pivotal.io>
2017-10-26 09:28:34 -0400

    Waiting for helm to init
    Signed-off-by: Felix Reyn <freyn@pivotal.io>

--- tasks/scripts/deploy_sk8s.sh
+++ tasks/scripts/deploy_sk8s.sh
@@ -38,6 +38,7 @@ kubectl create ns "$tiller_ns_name"
 kubectl create ns "$sk8s_ns_name"
 
 helm init --tiller-namespace="$tiller_ns_name"
+sleep 15 # wait for helm to be ready
 
 # clear out existing CRDs; safe to do in non-prod
 kubectl get customresourcedefinitions --all-namespaces -o json |
