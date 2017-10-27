30197d4613f4b1386b544b1cf156e5a7fecb7917
Felix Reyn <freyn@pivotal.io>
2017-10-27 14:54:18 -0400

    Add semver for sk8s versioning
    [https://github.com/pivotal-cf/pfs-ci/issues/11]

--- tasks/scripts/build_sk8s.sh
+++ tasks/scripts/build_sk8s.sh
@@ -4,11 +4,15 @@ set -exuo pipefail
 
 build_root=$PWD
 
+SK8S_VERSION=$(head "$build_root/sk8s-version/version")
+
 mkdir -p ~/.m2
 
 cp -r "$build_root/m2-repo/repository" ~/.m2/
 
 pushd $build_root/git-sk8s
   ./mvnw clean package -Ddocker.skip=true
+  ./mvnw versions:set -DnewVersion="$SK8S_VERSION"
   cp -r $build_root/git-sk8s/* "$build_root/sk8s-docker-contexts/"
+  echo "$SK8S_VERSION" > "$build_root/sk8s-docker-contexts/sk8s_version"
 popd
