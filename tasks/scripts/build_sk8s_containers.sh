717557c375682a0ff1df15ea1d8b5103d2a1d564
Felix Reyn <freyn@pivotal.io>
2017-11-14 14:39:42 -0500

    Cleaning up names

--- tasks/scripts/build_sk8s_containers.sh
+++ tasks/scripts/build_sk8s_containers.sh
@@ -14,6 +14,6 @@ cp -r "$build_root/m2-repo/repository" ~/.m2/
 pushd $build_root/git-sk8s
   ./mvnw versions:set -DnewVersion="$SK8S_VERSION"
   ./mvnw clean package -Ddocker.skip=true
-  cp -r $build_root/git-sk8s/* "$build_root/sk8s-docker-contexts/"
-  echo "$SK8S_VERSION" > "$build_root/sk8s-docker-contexts/sk8s_version"
+  cp -r $build_root/git-sk8s/* "$build_root/docker-contexts/"
+  echo "$SK8S_VERSION" > "$build_root/docker-contexts/sk8s_version"
 popd
