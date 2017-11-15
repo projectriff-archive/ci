79df31e7cb2aef19714c5a82dcea003f58baffd0
Felix Reyn <freyn@pivotal.io>
2017-11-15 10:04:25 -0500

    Increasing ZK-kafka start interval

--- tasks/containers/kafka_zookeeper/start_kafka.sh
+++ tasks/containers/kafka_zookeeper/start_kafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 zookeeper-server-start.sh /kafka/config/zookeeper.properties > /var/log/zookeeper.log &
-sleep 1
+sleep 5
 kafka-server-start.sh /kafka/config/server.properties > /var/log/kafka.log &
