e321679acaba4d2e72f1ca65ff6c4883c6e73c04
Felix Reyn <freyn@pivotal.io>
2017-12-14 13:30:01 -0500

    Fix handling of mainline riff and tiller NS names

--- tasks/scripts/common.sh
+++ tasks/scripts/common.sh
@@ -29,7 +29,7 @@ function generate_riff_ns_name(){
   local _riff_version="$2"
   local _suffix="$3"
   local _sanitized_version=$(echo "$_riff_version" | sed 's/\./-/g' |  awk '{print tolower($0)}')
-  echo "${_riff_name}-${_sanitized_version}-${_suffix}"
+  echo "main-${_riff_name}-${_sanitized_version}-${_suffix}"
 }
 
 function find_existing_tiller_ns(){
@@ -49,7 +49,7 @@ function find_existing_riff_ns(){
   local _sanitized_version=$(echo "$_riff_version" | sed 's/\./-/g' |  awk '{print tolower($0)}')
 
   set +e
-  existing_riff_ns_name=$(kubectl get ns | grep "${_riff_name}-${_sanitized_version}-" | awk '{print $1}')
+  existing_riff_ns_name=$(kubectl get ns | grep "main-${_riff_name}-${_sanitized_version}-" | awk '{print $1}')
   set -e
   echo "$existing_riff_ns_name"
 }
