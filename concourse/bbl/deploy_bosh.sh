002e8aef4a37a92cb0c7cbd2d2996037703c81d2
Felix Reyn <freyn@pivotal.io>
2018-01-04 11:12:30 -0500

    Updating bosh deployment script to fetch lpass info

--- concourse/bbl/deploy_bosh.sh
+++ concourse/bbl/deploy_bosh.sh
@@ -2,12 +2,16 @@
 
 set -exu -o pipefail
 
+lpass show --note 'Shared-pfs-eng/pfs-concourse-bbl-state' | base64 -D | gunzip > bbl-state.json
+lpass show --note 'Shared-pfs-eng/pfs-gcp-ci-svc' > pfs-ci-key.json
+
 bbl up \
   --state-dir $PWD \
   --gcp-zone us-central1-c \
   --gcp-region us-central1 \
-  --gcp-service-account-key \
-  ~/.gcloud/pfs-ci-key.json \
+  --gcp-service-account-key pfs-ci-key.json \
   --gcp-project-id cf-spring-pfs-eng \
   --iaas gcp \
   --debug
+
+rm pfs-ci-key.json
