89bcbb20f6e1525027223a88cf2731e7502c7b12
Jacques Chester <jchester@pivotal.io>
2018-02-09 15:29:00 -0500

    Show entrypoint logs
    Total silence, followed by failure, is a bit too terse.
    
    [Github projectriff/riff#258]

--- images/faas_cli_build/start_docker.sh
+++ images/faas_cli_build/start_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/bin/dockerd-entrypoint.sh >/tmp/docker.log 2>&1 &
+/usr/local/bin/dockerd-entrypoint.sh 2>&1 &
 until docker info >/dev/null 2>&1; do
   echo waiting for docker to come up...
   sleep 1
